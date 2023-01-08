be78281f3b8d20324e7f53d21fcae2758c96065d
toshiaki <conamn1185@gmail.com>
2023-01-08 22:25:22 +0900

    Airで生成されるtmpディレクトリがgitignoreされることを確認した

--- app/main.go
+++ app/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
+		fmt.Fprintf(w, "Hello WWW!")
 	})
 
 	http.ListenAndServe(":8080", nil)
