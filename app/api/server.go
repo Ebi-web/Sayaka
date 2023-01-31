3f6dadd6a3c4f4b8ac4f575115bc6fcb3a91c053
toshiaki <conamn1185@gmail.com>
2023-01-31 11:55:57 +0900

    ルーティング・DB導入

---
+++ app/api/server.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"Sayaka/api/handler"
+	"Sayaka/db"
+	"Sayaka/lib/alice"
+	"Sayaka/middlewares"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+)
+
+type Server struct {
+	db     *sqlx.DB
+	router *mux.Router
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Init(databaseDatasource string) error {
+	fmt.Println("ℹ️Server Initialising...")
+
+	sql := db.NewMySQL(databaseDatasource)
+	dbConnection, err := sql.Open()
+	if err != nil {
+		return fmt.Errorf("🚫failed db init. %s", err)
+	}
+	s.db = dbConnection
+
+	s.router = s.Route()
+	return nil
+}
+
+func (s *Server) Route() *mux.Router {
+	commonChain := alice.NewAliceChain()
+
+	m := middlewares.NewValidateSignatureMiddleware()
+	webhookChain := commonChain.Append(m.Handle)
+
+	webhookHandler := handler.NewWebhookHandler()
+
+	r := mux.NewRouter()
+	r.Methods(http.MethodGet).Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("200"))
+	})
+
+	r.Methods(http.MethodPost, http.MethodOptions).Path("/line/webhook").Handler(webhookChain.Then(AppHandler{webhookHandler.ResLineWebhook}))
+
+	r.Methods(http.MethodPost, http.MethodOptions).Path("/users").Handler(commonChain.Then(AppHandler{}))
+
+	return r
+}
+
+func (s *Server) Run(port int) {
+	log.Printf("🍺Listening on port %d", port)
+	if err := http.ListenAndServe(
+		fmt.Sprintf(":%d", port),
+		handlers.CombinedLoggingHandler(os.Stdout, s.router),
+	); err != nil {
+		panic(err)
+	}
+}
--- app/main.go
+++ app/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
 
+	"Sayaka/api"
 	"Sayaka/lib"
 	"Sayaka/lib/godotenv"
 )
 
 func main() {
+	var port int
+	var databaseDatasource string
+
+	flag.IntVar(&port, "port", 8080, "API server port")
+	flag.StringVar(&databaseDatasource, "databaseDatasource", "mysql://root:password@tcp(127.0.0.1:3306)/sayaka?sslmode=disable", "")
+	flag.Parse()
+
+	log.SetFlags(log.Ldate + log.Ltime + log.Lshortfile)
+	log.SetOutput(os.Stdout)
+
 	if err := preloadModules(libraries()); err != nil {
-		panic("External modules setup unsuccessful")
+		panic("🚫External modules setup unsuccessful")
 	}
 	fmt.Println("🎉Successful external modules setup")
 
-	server := NewServer()
-	server.Init()
+	server := api.NewServer()
+	if err := server.Init(databaseDatasource); err != nil {
+		log.Fatal(err)
+	}
+	server.Run(port)
 }
 
 func libraries() []lib.Preparer {
