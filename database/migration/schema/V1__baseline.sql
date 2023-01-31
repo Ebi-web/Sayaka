0059ccf08bc24104f7a84a300d5eb255772093bc
toshiaki <conamn1185@gmail.com>
2023-01-31 15:56:12 +0900

    マイグレーション修正

--- database/migration/schema/V1__baseline.sql
+++ database/migration/schema/V1__baseline.sql
@@ -4,11 +4,11 @@ use sayaka;
 CREATE TABLE users
 (
     id           INT AUTO_INCREMENT,
-    line_user_id VARCHAR(255) NOT NULL,
-    display_name VARCHAR(255) NOT NULL,
+    line_user_id VARCHAR(255)                                                   NOT NULL,
+    display_name VARCHAR(255)                                                   NOT NULL,
     photo_url    VARCHAR(255),
-    created_at   DATETIME     NOT NULL,
-    updated_at   DATETIME     NOT NULL,
+    created_at   DATETIME DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
+    updated_at   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -18,8 +18,8 @@ CREATE TABLE flash_cards
     user_id    INT          NOT NULL,
     front      VARCHAR(255) NOT NULL,
     back       VARCHAR(255) NOT NULL,
-    created_at DATETIME     NOT NULL,
-    updated_at DATETIME     NOT NULL,
+    created_at   DATETIME DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
+    updated_at   DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY fk_user_id (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
